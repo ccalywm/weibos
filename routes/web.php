b45a4778b134f9c7b271ef8266fa7842f652b3eb
vagrant <vagrant@homestead>
2019-07-06 08:09:26 +0000

    路由命名

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@
 //     return view('welcome');
 // });
 //三个静态路由 分别返回首页、帮助页和关于页面
-Route::get('/','StaticPagesController@home');
-Route::get('/help','StaticPagesController@help');
-Route::get('/about','StaticPagesController@about');
+Route::get('/','StaticPagesController@home')->name('home');
+Route::get('/help','StaticPagesController@help')->name('help');
+Route::get('/about','StaticPagesController@about')->name('about');
 
