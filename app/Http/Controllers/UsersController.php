566aa40d88a7e02f65af2ac5bb2e354cb85904f1
vagrant <vagrant@homestead>
2019-07-08 09:32:06 +0000

    注册功能完成

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -19,14 +19,25 @@ class UsersController extends Controller
         return view('users.show',compact('user'));
     }
 
-    //验证注册表单
+    //注册表单验证
     public function store(Request $request)
     {
+        //验证输入
         $this->validate($request,[
             'name' => 'required|max:50',
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:3',
         ]);
-        return;
+
+        //调用方法写入数据库
+        $user = User::create([
+           'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        //写入完成之后返回用户页面并且提示
+        session()->flash('success','欢迎注册，您将在这里开启一度新的旅程');
+        return redirect()->route('users.show',[$user]);
     }
 }
