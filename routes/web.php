591b1fa478a950bfd339ee3754664436bdfb7bf9
vagrant <vagrant@homestead>
2019-07-08 08:37:56 +0000

    用户界面显示

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,15 @@ Route::get('/about','StaticPagesController@about')->name('about');
 
 Route::get('/signup','UsersController@create')->name('signup');
 
+/**
+ * resource 路由等于下面注释掉的路由
+ * Route::get('/users', 'UsersController@index')->name('users.index');
+ * Route::get('/users/create', 'UsersController@create')->name('users.create');
+ * Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+ * Route::post('/users', 'UsersController@store')->name('users.store');
+ * Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+ * Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+ * Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
+ */
+Route::resource('users','UsersController');
+
