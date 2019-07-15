13f8e1c374bfe9a549d295718fe5a7e1436ff288
vagrant <vagrant@homestead>
2019-07-15 07:28:48 +0000

    微博数据模型

---
+++ database/migrations/2019_07_15_151226_create_statuses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statuses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');//收入&&支出
+            $table->integer('jine');//金额
+            $table->text('content');//备注
+            $table->time('time');//时间
+            $table->integer('user_id')->index();
+            $table->index(['created_at']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statuses');
+    }
+}
